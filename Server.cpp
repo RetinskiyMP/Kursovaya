5ce648b96e5325793a045d9970dcdee572cb2c17
RetinskiyMP <39593410+RetinskiyMP@users.noreply.github.com>
2018-11-05 21:04:33 +0700

    +ООП
    Добавлено чутка ООП

--- Server.cpp
+++ Server.cpp
@@ -6,12 +6,73 @@
 
 using namespace std;
 
-DWORD WINAPI Func(LPVOID client_socket)
+class Server
+{
+protected:
+	SOCKET s;//Для сервера
+	WSADATA WsaData;
+	SOCKADDR_IN sin;
+	SOCKET client_socket; // сокет для клиента
+	SOCKADDR_IN client_addr; // адрес клиента (заполняется системой)
+public:
+	Server();
+	~Server();
+	void GetServerSettings();
+	void StartServer();
+	void StartListenning();
+	static DWORD WINAPI Func(LPVOID);
+};
+
+Server::Server()
+{}
+Server::~Server()
+{}
+
+void Server::GetServerSettings()
+{
+	WSAStartup(0x0101, &WsaData); //Инициализируем процесс библиотеки ws2_32, вызвав функцию WSAStartup
+	
+	s = socket(AF_INET, SOCK_STREAM, 0); //Теперь объявление переменную типа SOCKET
+
+	//Задаем параметры для сокета(сервера)
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(7770);
+	sin.sin_addr.s_addr = INADDR_ANY;
+
+	cout << "Сервер успешно настроен." << endl;
+}
+
+void Server::StartServer()
+{
+	bind(s, (LPSOCKADDR)&sin, sizeof(sin));
+	cout << "Сервер успешно запущен." << endl;
+}
+
+void Server::StartListenning()
+{
+	listen(s, SOMAXCONN); //Ждем подключений
+
+	int client_addr_size = sizeof(client_addr);
+
+	cout << "Ожидание подключения..." << endl;
+
+	while ((client_socket = accept(s, (sockaddr *)&client_addr, \
+		&client_addr_size)))
+	{
+		cout << "Сокет клиента [" << client_socket << "] подключен, обработка..." << endl;
+
+		DWORD thID;
+		CreateThread(NULL, NULL, Func, &client_socket, NULL, &thID);
+	}
+
+}
+
+DWORD WINAPI Server::Func(LPVOID client_socket)
 {
 	SOCKET my_sock;
 	my_sock = ((SOCKET *)client_socket)[0];
-	char buff[BUFSIZ];//char buff[20 * 1024];
-#define sHELLO "SOCKET PODKLUCHEN\r\n"
+	char buff[BUFSIZ]; //char buff[20 * 1024];
+#define sHELLO "Сокет подключен\r\n"
 
 // отправляем клиенту приветствие
 	send(my_sock, sHELLO, sizeof(sHELLO), 0);
@@ -20,79 +81,27 @@ DWORD WINAPI Func(LPVOID client_socket)
 	int bytes_recv;
 	while ((bytes_recv = recv(my_sock, &buff[0], sizeof(buff), 0)) &&
 		bytes_recv != SOCKET_ERROR)
-		//*tmp_buff = buff[0];
-		//changeWords();
-
+	{
 		send(my_sock, &buff[0], bytes_recv, 0);
+	}
 
+	cout << "Соединение с сокетом клиента [" << my_sock << "] разорвано." << endl;
 	// если мы здесь, то произошел выход из цикла по причине
 	// возращения функцией recv ошибки - соединение с клиентом разорвано
-	
-		// закрываем сокет
-		closesocket(my_sock);
+
+	// закрываем сокет
+	closesocket(my_sock);
 	return 0;
 }
 
 int main()
 {
-	SOCKET s;//Для сервера
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
 
-	//Инициализируем процесс библиотеки ws2_32, вызвав функцию WSAStartup
-	WSADATA WsaData;
-	int err = WSAStartup(0x0101, &WsaData); //0х0101 - версия
-	if (err == SOCKET_ERROR)
-	{
-		cout << "Error";
-		return 1;
-	}
-	//Теперь объявление переменную типа SOCKET
-	s = socket(AF_INET, SOCK_STREAM, 0);
+	Server Server1;
 
-	//Задаем параметры для сокета(сервера)
-	SOCKADDR_IN sin;
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(7770);
-	sin.sin_addr.s_addr = INADDR_ANY;
-
-	//Запускаем сервер
-	err = bind(s, (LPSOCKADDR)&sin, sizeof(sin));
-	if (err == 0)
-	{
-		cout << "Server starting..." << endl;
-	}
-	else
-	{
-		cout << "Error";
-		return 1;
-	}
-
-	//Ждем подключений 
-	err = listen(s, SOMAXCONN);
-	if (err == 0)
-	{
-		cout << "Listening..." << endl;
-	}
-	else
-	{
-		cout << "Error";
-		return 1;
-	}
-
-	//Обработка подключений
-	SOCKET client_socket; // сокет для клиента
-	sockaddr_in client_addr; // адрес клиента (заполняется системой)
-
-	int client_addr_size = sizeof(client_addr);
-	char buffer[] = "connect - ok";
-
-	while ((client_socket = accept(s, (sockaddr *)&client_addr, \
-		&client_addr_size)))
-	{
-		cout << "Client is connected.";
-
-		DWORD thID;
-		CreateThread(NULL, NULL, Func, &client_socket, NULL, &thID);
-
-		send(s, buffer, sizeof(buffer), NULL);
-	}
+	Server1.GetServerSettings();
+	Server1.StartServer();
+	Server1.StartListenning();
 }
